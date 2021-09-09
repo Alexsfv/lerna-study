8d9250889d52d4e3757621db6675577ba6fa04e3
Alexsfv <safronov.sanya37@gmail.com>
2021-09-09 10:24:40 +0500

    first commit

---
+++ workspaces/one/src/index.js
@@ -0,0 +1,3 @@
+const { v4: uuidv4 } = require('uuid');
+
+module.exports = `"Module 1-1" id: ${uuidv4()}`
---
+++ workspaces/three/src/index.js
@@ -0,0 +1,4 @@
+let one = require("@lern/one")
+let two = require("@lern/two")
+
+console.log(`third module: ${one}, ${two}`)
